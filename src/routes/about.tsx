cc592df8641db3412e6a2d58bb8d2355140ee196
Marc Obel <mao@syseng.dk>
2025-06-13 22:01:18 +0200

    a

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,7 +1,25 @@
 export const Route = createFileRoute({
   component: About,
-})
+});
 
 function About() {
-  return <pre>Hello from About!</pre>
+  return (
+    <div>
+      <h3>About</h3>
+      <p>
+        The idea is a simple turn based grid game where all state lives in
+        search params of the browser.
+      </p>
+      <p>Benefits of this approach</p>
+      <ul>
+        <li>Easy to share the game state with others</li>
+        <li>
+          Easy to save the game state, fx with bookmarks or copy to clipboard
+        </li>
+
+        <li>Undo functionality would be free, just go back in browser</li>
+        <li>Easy to debug the game state</li>
+      </ul>
+    </div>
+  );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,26 @@
+import { Link } from "@tanstack/react-router";
+import { z } from "zod";
+
+const gameStateSchema = z.object({
+  px: z.number().catch(1),
+  py: z.number().catch(1),
+});
+
+// type GameState = z.infer<typeof gameStateSchema>;
+
 export const Route = createFileRoute({
+  validateSearch: (search) => gameStateSchema.parse(search),
   component: Index,
-})
+});
 
 function Index() {
+  const { px, py } = Route.useSearch();
   return (
-    <div>
-      <pre>Welcome Home!</pre>
-    </div>
-  )
+    <main>
+      <Link to="/about" className="helpbtn" >?</Link>
+      <pre>
+        {`Game state: px=${px}, py=${py}`}
+      </pre>
+    </main>
+  );
 }
