89680026c7b7ae9c68e91d0c438708ebd37c1c1d
Marc Obel <mao@syseng.dk>
2025-06-13 21:07:53 +0200

    Updates

---
+++ assets/about-BB0W9o-v.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-B5h8hGRo.js";const r=function(){return o.jsx("div",{children:"Hello from About!"})};export{r as component};
--- assets/index-ByBHW5CN.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-tsxplW-V.js";const t=function(){return n.jsx("div",{children:n.jsx("h3",{children:"Welcome Home!"})})};export{t as component};
