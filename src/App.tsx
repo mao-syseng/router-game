8f6dc978ce39bd9d1c93a3fb7a7dd3f90ca85e0c
Marc Obel <mao@syseng.dk>
2025-10-03 18:43:54 +0200

    a

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useQueryState } from "nuqs";
+import glitchSamuraiIdle from "./assets/glitchSamuraiIdlei.gif";
+
+function App() {
+  const [map, setMap] = useQueryState("m", { defaultValue: "ec" });
+  return (
+    <>
+      <div className="grid">
+        <div className="cell">
+          <img
+            src={glitchSamuraiIdle}
+            className="logo react"
+            alt="React logo"
+          />
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+        odio a dignissimos, quasi adipisci veniam quidem voluptatum dolor in
+        vel. Quo, non. Exercitationem eius quod cumque blanditiis est unde
+        temporibus, quo obcaecati beatae ab, asperiores delectus adipisci
+        laudantium nisi reprehenderit veniam. Excepturi repellat dolor rerum
+        exercitationem sapiente nostrum hic quia quae libero, at atque
+        accusantium similique ipsum architecto! Eius, quas magni architecto
+        inventore tenetur nostrum sapiente doloribus nulla repellendus! Ad
+        obcaecati blanditiis sunt eligendi, iusto inventore quasi ipsum ut ex
+        quibusdam suscipit neque tenetur quos quaerat perferendis quidem
+        assumenda illum tempore sit dolore pariatur culpa. Repellendus
+        reiciendis nisi inventore laborum?
+      </p>
+    </>
+  );
+}
+
+export default App;
--- src/components/HelpBtn.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "@tanstack/react-router";
-
-export default function HelpBtn() {
-  return (
-    <Link to="/about" className="helpbtn">
-      ?
-    </Link>
-  );
-}
